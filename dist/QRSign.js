a84f2c76410e5beca101d0bfa8430b28fb4f61e2
maniacata <maniaciachao@gmail.com>
2020-11-10 12:42:02 +0800

    :bookmark: bump version to 1.0.0

---
+++ dist/QRSign.js
@@ -0,0 +1,207 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.QRSign = void 0;
+var ws_1 = __importDefault(require("ws"));
+var qrcode_1 = require("qrcode");
+var requests_1 = require("./requests");
+var consts_1 = require("./consts");
+var QRType;
+(function (QRType) {
+    QRType[QRType["default"] = 0] = "default";
+    QRType[QRType["code"] = 1] = "code";
+    QRType[QRType["unknown"] = 2] = "unknown";
+    QRType[QRType["result"] = 3] = "result";
+})(QRType || (QRType = {}));
+var QRSign = /** @class */ (function () {
+    function QRSign(info) {
+        var _this = this;
+        // fields
+        this._seqId = 0;
+        this.clientId = "";
+        this.client = null;
+        this.interval = 0;
+        this.onSuccess = null;
+        //
+        this.sendMessage = function (msg) {
+            var _a;
+            var raw = JSON.stringify(msg ? [msg] : []);
+            (_a = _this.client) === null || _a === void 0 ? void 0 : _a.send(raw);
+        };
+        this.handleMessage = function (data) {
+            var _a;
+            try {
+                var messages = JSON.parse(data);
+                // heartbeat response
+                if (Array.isArray(messages) && messages.length === 0) {
+                    return;
+                }
+                var message = messages[0];
+                var _b = message, channel = _b.channel, successful = _b.successful;
+                if (!successful) {
+                    // qr subscription
+                    if (/attendance\/\d+\/\d+\/qr/.test(channel)) {
+                        console.log(channel + ": successful!");
+                        var data_1 = message.data;
+                        console.log(message);
+                        switch (data_1.type) {
+                            case QRType.code: {
+                                if (consts_1.config.shortUrl) {
+                                    requests_1.tryShortenURL(data_1.qrUrl).then(function (url) {
+                                        qrcode_1.toString(url, { type: "terminal" }).then(function (qr) { return console.log(qr); });
+                                    });
+                                }
+                                else {
+                                    qrcode_1.toString(data_1.qrUrl, { type: "terminal" }).then(function (qr) {
+                                        return console.log(qr);
+                                    });
+                                }
+                                break;
+                            }
+                            case QRType.result: {
+                                var student = data_1.student;
+                                if (student && student.name === consts_1.config.name) {
+                                    (_a = _this.onSuccess) === null || _a === void 0 ? void 0 : _a.call(_this, student);
+                                }
+                                break;
+                            }
+                            default:
+                                break;
+                        }
+                    }
+                    else {
+                        throw channel + ": failed!";
+                    }
+                }
+                else {
+                    console.log(channel + ": successful!");
+                    switch (message.channel) {
+                        case "/meta/handshake": {
+                            var clientId = message.clientId;
+                            _this.clientId = clientId;
+                            _this.connect();
+                            break;
+                        }
+                        case "/meta/connect": {
+                            var timeout = message.advice.timeout;
+                            _this.startHeartbeat(timeout);
+                            _this.subscribe();
+                            break;
+                        }
+                        case "/meta/subscribe": {
+                            break;
+                        }
+                        default: {
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (err) {
+                console.log("QR: " + err);
+            }
+        };
+        this.handshake = function () {
+            return _this.sendMessage({
+                channel: "/meta/handshake",
+                version: "1.0",
+                supportedConnectionTypes: [
+                    "websocket",
+                    "eventsource",
+                    "long-polling",
+                    "cross-origin-long-polling",
+                    "callback-polling",
+                ],
+                id: _this.seqId,
+            });
+        };
+        this.connect = function () {
+            return _this.sendMessage({
+                channel: "/meta/connect",
+                clientId: _this.clientId,
+                connectionType: "long-polling",
+                id: _this.seqId,
+                advice: {
+                    timeout: 0,
+                },
+            });
+        };
+        this.startHeartbeat = function (timeout) {
+            _this.sendMessage();
+            _this.interval = setInterval(_this.sendMessage, timeout);
+        };
+        this.subscribe = function () {
+            return _this.sendMessage({
+                channel: "/meta/subscribe",
+                clientId: _this.clientId,
+                subscription: "/attendance/" + _this.courseId + "/" + _this.signId + "/qr",
+                id: _this.seqId,
+            });
+        };
+        this.courseId = info.courseId;
+        this.signId = info.signId;
+    }
+    QRSign.prototype.start = function (cb) {
+        var _this = this;
+        this.onSuccess = cb;
+        this.client = new ws_1.default(QRSign.endpoint);
+        this.client.on("open", function () {
+            _this.handshake();
+        });
+        this.client.on("message", function (data) {
+            // console.log(data);
+            _this.handleMessage(data.toString());
+        });
+    };
+    QRSign.prototype.destory = function () {
+        var _a;
+        clearInterval(this.interval);
+        (_a = this.client) === null || _a === void 0 ? void 0 : _a.close();
+    };
+    Object.defineProperty(QRSign.prototype, "seqId", {
+        // getters
+        get: function () {
+            return "" + this._seqId++;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    // static
+    QRSign.endpoint = "wss://www.teachermate.com.cn/faye";
+    return QRSign;
+}());
+exports.QRSign = QRSign;
+var sign = new QRSign({ courseId: 1164213, signId: 1983462 });
+// [
+//   {
+//     channel: "/meta/handshake",
+//     version: "1.0",
+//     supportedConnectionTypes: [
+//       "websocket",
+//       "eventsource",
+//       "long-polling",
+//       "cross-origin-long-polling",
+//       "callback-polling",
+//     ],
+//     id: "1",
+//   },
+// ][
+//   {
+//     channel: "/meta/connect",
+//     clientId: "c6jlqzd5391xri99f3qt0a3xpa6uqur",
+//     connectionType: "long-polling",
+//     id: "2",
+//     advice: {
+//       timeout: 0,
+//     },
+//   }
+// ][
+//   {
+//     channel: "/meta/subscribe",
+//     clientId: "c6jlqzd5391xri99f3qt0a3xpa6uqur",
+//     subscription: "/attendance/1164213/1982993/qr",
+//     id: "3",
+//   }
+// ];
--- dist/index.js
+++ dist/index.js
@@ -11,12 +11,20 @@ var __assign = (this && this.__assign) || function () {
     return __assign.apply(this, arguments);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var requests_1 = require("./requests");
 var readline_sync_1 = require("readline-sync");
+var node_notifier_1 = require("node-notifier");
+var process_1 = require("process");
+var requests_1 = require("./requests");
 var consts_1 = require("./consts");
+var QRSign_1 = require("./QRSign");
 var extractOpenId = function (str) { var _a; return str.length === 32 ? str : (_a = str.match("openid=(.*)(?=&)")) === null || _a === void 0 ? void 0 : _a[1]; };
-var openId = extractOpenId(readline_sync_1.question("Paste openId or URL here: "));
-var signIdSet = new Set();
+var sendNotificaition = function (message) {
+    return node_notifier_1.notify({ message: message, title: "yatm" });
+};
+var openId = extractOpenId(process_1.env.OPEN_ID ? process_1.env.OPEN_ID : readline_sync_1.question("Paste openId or URL here: "));
+var signedIdSet = new Set();
+var lastSignId = 0;
+var qrSign;
 if (openId) {
     setInterval(function () {
         requests_1.activeSign(openId)
@@ -24,25 +32,49 @@ if (openId) {
             .then(function (data) {
             var signId = data.signId, courseId = data.courseId, isGPS = data.isGPS, isQR = data.isQR;
             if (!courseId || !signId) {
+                qrSign === null || qrSign === void 0 ? void 0 : qrSign.destory();
                 throw "No sign available";
             }
-            if (signIdSet.has(signId)) {
+            if (signedIdSet.has(signId)) {
                 throw "already signed";
             }
-            console.log("current sign:", data);
-            var signInQuery = { courseId: courseId, signId: signId };
-            if (data.isGPS) {
-                signInQuery = __assign(__assign({}, signInQuery), { lat: 30, lon: 30 });
-            }
-            requests_1.signIn(openId, signInQuery)
-                .then(function (data) { return data.json(); })
-                .then(function (data) {
-                if (!data.errorCode || data.errorCode === 305) {
-                    signIdSet.add(signId);
+            sendNotificaition("Info: a sign is going on!");
+            if (isQR) {
+                if (signId === lastSignId) {
+                    return;
                 }
-                console.log(data);
-            });
+                lastSignId = signId;
+                sendNotificaition("WARNING: QR sign is going on!");
+                qrSign === null || qrSign === void 0 ? void 0 : qrSign.destory();
+                qrSign = new QRSign_1.QRSign({ courseId: courseId, signId: signId });
+                qrSign.start(function (result) {
+                    console.log(result);
+                    signedIdSet.add(signId);
+                });
+                console.warn("QR sign successfully. However, you need to re-run this script with NEW openid!");
+            }
+            else {
+                console.log("current sign:", data);
+                var signInQuery = { courseId: courseId, signId: signId };
+                if (isGPS) {
+                    signInQuery = __assign(__assign({}, signInQuery), { lat: 30, lon: 30 });
+                }
+                requests_1.signIn(openId, signInQuery)
+                    .then(function (data) { return data.json(); })
+                    .then(function (data) {
+                    if (!data.errorCode || data.errorCode === 305) {
+                        signedIdSet.add(signId);
+                    }
+                    console.log(data);
+                })
+                    .catch(function (e) {
+                    console.log(e);
+                    sendNotificaition("Error: failed to sign in. See output plz.");
+                });
+            }
         })
-            .catch(function (e) { return console.log(e); });
+            .catch(function (e) {
+            console.log(e);
+        });
     }, consts_1.config.interval);
 }
